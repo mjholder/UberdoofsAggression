e6d175d47a8db030c25e662f8229da94380a81b2
Matthew Holder <mjholder96@gmail.com>
2020-11-16 22:05:48 -0500

    Initial commit
    Basic UI and Photon functionality

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorMessageText;
+    [SerializeField] TMP_Text roomNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Gets called as soon as we connect to the Photon master server
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("Title");
+        Debug.Log("Joined lobby");
+    }
+
+    public void CreateRoom()
+    {
+        // entering "" in CreateRoom() will create a room with a random name
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("RoomMenu");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorMessageText.text = "Room creation failed: " + message;
+        MenuManager.Instance.OpenMenu("ErrorMenu");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("Title");
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    public bool open;
+
+    public void Open()
+    {
+        open = true;
+        gameObject.SetActive(true);
+    }
+
+    public void Closed()
+    {
+        open = false;
+        gameObject.SetActive(false);
+    }
+}
